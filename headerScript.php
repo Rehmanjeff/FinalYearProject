083cdcf0ffe0e955d57b8ab2633c3d06fadc2d2d
Habib ur Rehman <rehmank360@gmail.com>
2015-09-17 18:23:23 +0500

    Working on Researcher_Profile Backend

--- headerScript.php
+++ headerScript.php
@@ -8,8 +8,9 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+-->
+<script src="js/jquery.min.js"></script>
     <script src="js/jquery-1.2.6.min"></script>
     <script src="bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 
--- teacher_profile_edits.php
+++ teacher_profile_edits.php
@@ -10,17 +10,10 @@
                     <?php include('header.php'); ?>
                 </header>
             </div>
-        <div class="row" id="inner_row">
-            <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
-                <li class="active"><a href=".biography" role="tab" data-toggle="tab">Biography</a></li>
-                <li><a href=".profile" role="tab" data-toggle="tab">Academic Resource</a></li>
-            </ul>
-            <!-- Tab panes -->
-            <div class="tab-content">
-                <div class="tab-pane active biography">
-                    <div class="row" id="first_row">
-                        <br>
+            <div>
+                <div class="row" id="inner_row">
+                    <br>
+                    <div id="first_row">
                         <div class="col-md-2">
                             <img src="http://www.w3schools.com/bootstrap/cinqueterre.jpg" class="img-circle" alt="Cinque Terre">
                         </div>
@@ -36,7 +29,6 @@
                                         <select>
                                             <option>Professor</option>
                                             <option>Associate Professor</option>
-                                            <option>Assistant Professor</option>
                                             <option>Senior Lecturer</option>
                                             <option>Lecturer</option>
                                         </select>
@@ -45,14 +37,42 @@
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="comment">About Yourself:</label>
-                                <textarea class="form-control" rows="5" id="comment"></textarea>
+                            <div id="contact_info">
+                                <h4>Contact Information</h4>
+
+                                    <div id="contact_name">
+                                        <input type="email" placeholder="Enter Email Address" class="form-control" id=""><br>
+                                        <input type="number" placeholder="Enter Phone Number" class="form-control" id=""><br>
+                                        <input type="number" placeholder="Enter Fax" class="form-control" id="">
+                                    </div>
                             </div>
                         </div>
                     </div>
+                    </div>
+                </div>
+            </div>
+        <div class="row" id="">
+            <br>
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+                <li class="active"><a href=".biography" role="tab" data-toggle="tab">Biography</a></li>
+                <li><a href=".publication" role="tab" data-toggle="tab">Publications</a></li>
+                <li><a href=".supervision" role="tab" data-toggle="tab">Supervision</a></li>
+                <li><a href=".projects" role="tab" data-toggle="tab">Projects</a></li>
+                <li><a href=".awards" role="tab" data-toggle="tab">Awards</a></li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div class="tab-pane active biography">
                     <div class="row" id="second_row">
                         <br>
+                        <div>
+                            <h3>About Yourself: </h3>
+                            <div class="form-group">
+                                <label for="comment"></label>
+                                <textarea class="form-control" rows="5" id="comment"></textarea>
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-md-3" style="text-align:">
                                 <h3>Qualification</h3>
@@ -98,6 +118,46 @@
                             </div>
                         </div>
                     </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="projects_backend">
+                                    <h3>Projects: </h3>
+                                    <div id="nav_projects_outer" class="col-md-9">
+                                        <div id="all_done" class="col-md-8">
+                                            <div id="project_name">
+                                                <h4>Project Name <small>(Supervisor)</small></h4>
+                                                <h4>HCI</h4>
+                                            </div>
+                                            <div style="clear: both;"></div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis in nunc vel cursus.
+                                                Proin elementum pellentesque laoreet. Nam vitae orci ullamcorper, luctus justo ac, dapibus ipsum.
+                                                Nam laoreet, sem in luctus dapibus, tellus enim luctus ex, nec aliquet enim nulla sit amet lectus.
+                                                In hac habitasse platea dictumst. Maecenas lectus massa, semper nec lacinia ac, mollis vel leo.
+                                                Nulla vestibulum volutpat sapien quis viverra. Etiam vitae elementum sapien, sed finibus odio.
+                                                Sed sed commodo neque.
+                                            </p>
+                                            <div style="border:1px black;" class="col-md-7">
+                                                <p><strong> Sponsor name </strong></p>
+                                            </div>
+                                            <div><span class="label label-success">Done</span></div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <br>
+                            <div class="save_btn">
+                                <button class="btn btn-group-sm btn-success">Save</button>
+                            </div>
+                        </div>
+                <div class="tab-pane profile">
+                    profile
+                </div>
+                <div class="tab-pane publication">
+
                     <div class="row" id="Third_row">
                         <br>
                         <div class="row">
@@ -109,45 +169,25 @@
                         <div class="row">
                             <br>
                             <div id="add_publications">
-                                <button type="button" class="btn btn-info btn-primary glyphicon-plus" data-toggle="modal" data-target="#myModal"> Publication </button>
+                                <button type="button" style = "margin-left:12px; " class="btn btn-sm btn-primary glyphicon-plus" data-toggle="modal" data-target="#myModal"> Publication </button>
                                 <div id="myModal" class="modal fade" role="dialog">
                                     <div class="modal-dialog">
                                         <!-- Modal content-->
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">News Update</h4>
+                                                <h4 class="modal-title">Add Publications</h4>
                                             </div>
 
                                             <div class="modal-body">
                                                 <div class="form-group">
-                                                    <label for="usr">Title:</label>
-                                                    <input type="text" class="form-control" id="usr">
-                                                </div>
-                                                <br>
-
-                                                <div class="form-group">
-                                                    <label for="comment">Description: </label>
+                                                    <label for="comment">Description:</label>
                                                     <textarea class="form-control" rows="5" id="comment"></textarea>
                                                 </div>
-
-                                                <div class='col-md-6' style="float: left;">
-                                                    <div class="form-group">
-                                                        <label class="control-label" for="exampleInputEmail">Pick Date:</label>
-                                                        <input name="DATEFROM" id="dateFrom" type="date" class="form-control" />
-                                                    </div>
-
-                                                    <script type="text/javascript">
-                                                        $(document).ready(function ()
-                                                        {
-                                                            $('#dateFrom').datepicker({
-                                                                format: "dd/mm/yyyy",
-                                                                orientation: "top"});
-                                                        });
-                                                    </script>
-                                                    <br><br><br><br>
+                                                <div class="form-group">
+                                                    <label for="usr">Link:</label>
+                                                    <input type="text" placeholder="Provide link for publication" class="form-control" id="usr">
                                                 </div>
-
                                                 <div class="modal-footer">
                                                     <button class="btn btn-warning" data-dismiss="modal">Close</button>
                                                     <button class="btn btn-success" type="submit" data-dismiss="modal">Submit</button>
@@ -156,18 +196,20 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <br>
-                            <div class="save_btn">
-                                <button class="btn btn-group-sm btn-success">Save</button>
+                                <div class="col-md-8">
+                                    <h4>Description:</h4>
+                                    <p id="para">
+                                        Building on our earlier work and that of Son, we construct string theory duals of non-relativistic critical phenomena at finite temperature and density. Concretely, we find black hole solutions of type IIB supergravity whose asymptotic geometries realize the Schroedinger group as isometries. We then identify the non-relativistic conformal field theories to which they are dual. We analyze the thermodynamics of these black holes, which turn out to describe the system at finite temperature and finite density. The strong-coupling result for the shear viscosity of the dual non-relativistic field theory saturates the KSS bound.
+                                    </p>
+                                    <input type="button" onclick="return toggleMe('para')" value="view more">
+                                    <br><br>
+                                    <strong>Website Link:</strong><a href="http://arxiv.org/abs/0807.1111">A. Adams, K. Balasubramanian, J. McGreevy</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                <div class="tab-pane profile">
-                    profile
                 </div>
-                </div>
+            </div>
             <hr>
             </div>
         </div>
